14ee6cbcb6ce6cb4a61eb840cb8d108348303d98
Alexa <alexakearns@outlook.com>
2020-05-18 16:04:28 +0100

    withdraw method decreases balance - Red

--- spec/ClientSpec.js
+++ spec/ClientSpec.js
@@ -14,3 +14,9 @@ it('should increase balance when depositing funds', function() {
     client.deposit(25);
     expect(client.balance).toEqual(25);
 });
+
+it('should decrease balance when withdrawing funds', function() {
+    client.deposit(25)
+    client.withdraw(5)
+    expect(client.balance).toEqual(5)
+})
