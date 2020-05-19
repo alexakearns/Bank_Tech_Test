a51ca805cd8c3b73aed8afdb86240dd14eef97cf
Alexa <alexakearns@outlook.com>
2020-05-19 13:02:57 +0100

    initialise transaction with debit - Green

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,10 +3,11 @@
 # class for each client transction
 class Transaction
 
-  attr_reader :balance, :credit
+  attr_reader :balance, :credit, :debit
 
-  def initialize(balance, credit)
+  def initialize(balance, credit, debit)
     @balance = balance
     @credit = credit
+    @debit = debit
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,12 +5,12 @@ require 'transaction'
 describe Transaction do
 
   it 'should initialise with balance properties' do
-    transaction = Transaction.new(100, 50) 
+    transaction = Transaction.new(100, 50, 0) 
     expect(transaction.balance).to eq 100
   end
 
   it 'should initialise with credit properties' do
-    transaction = Transaction.new(100, 50) 
+    transaction = Transaction.new(100, 50, 0) 
     expect(transaction.credit).to eq 50
   end
 
