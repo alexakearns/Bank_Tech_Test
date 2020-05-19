c78d30d230986d370c2f660d167942033290c0b3
Alexa <alexakearns@outlook.com>
2020-05-19 12:53:38 +0100

    initialise transaction with balance - Red

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,4 +4,8 @@ require 'transaction'
 
 describe Transaction do
 
+  it 'should initialise with balance, credit and debit properties' do
+    transaction = Transaction.new(balance: 100) 
+    expect(transaction.balance).to eq 100
+  end
 end
