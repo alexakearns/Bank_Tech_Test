d57a077ea648d25815b93283cc64bd7e57bf1f55
Alexa <alexakearns@outlook.com>
2020-05-19 13:02:08 +0100

    initialise transaction with debit - Red

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -13,4 +13,9 @@ describe Transaction do
     transaction = Transaction.new(100, 50) 
     expect(transaction.credit).to eq 50
   end
+
+  it 'should initialise with credit properties' do
+    transaction = Transaction.new(100, 0, 25) 
+    expect(transaction.debit).to eq 25
+  end
 end
