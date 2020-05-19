756395f9c43a3e39023bbfeca4e766f2594eb761
Alexa <alexakearns@outlook.com>
2020-05-19 12:18:14 +0100

    client deposits funds and increases balance - Red

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,4 +5,9 @@ describe Client do
   it 'should have an initial balance of 0' do
     expect(subject.current_balance).to eq 0
   end
+
+  it 'should deposit funds and increase current balance' do
+    subject.deposit(100)
+    expect(subject.current_balance).to eq 100
+  end
 end
