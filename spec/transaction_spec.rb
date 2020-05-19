3bc940abde120946aa50862dbb9a6df860994edc
Alexa <alexakearns@outlook.com>
2020-05-19 15:03:34 +0100

    transaction display return for both debit and credit - Green

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -19,8 +19,16 @@ describe Transaction do
     expect(transaction.debit).to eq 25
   end
 
-  it 'should display transaction' do
+  it 'should display deposit transaction' do
     transaction = Transaction.new(balance: 100, credit: 40)
     expect(transaction.display).to eq 'balance: 100 | credit: 40 | debit: '
   end
+
+  it 'should display withdraw transaction' do
+    transaction = Transaction.new(balance:100, debit: 30)
+    expect(transaction.display).to eq 'balance: 100 | credit:  | debit: 30'
+  end
+    
+
+
 end
