be07902c6307a18d41153fac0b548c30d3ca4bdc
Alexa <alexakearns@outlook.com>
2020-05-19 13:00:38 +0100

    add credit to transaction when initialising - Green

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,9 +3,10 @@
 # class for each client transction
 class Transaction
 
-  attr_reader :balance
+  attr_reader :balance, :credit
 
-  def initialize(balance)
+  def initialize(balance, credit)
     @balance = balance
+    @credit = credit
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,7 +5,7 @@ require 'transaction'
 describe Transaction do
 
   it 'should initialise with balance properties' do
-    transaction = Transaction.new(100) 
+    transaction = Transaction.new(100, 50) 
     expect(transaction.balance).to eq 100
   end
 
