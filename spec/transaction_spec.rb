b0a5aadb791904da458c4c4657612990265204dc
Alexa <alexakearns@outlook.com>
2020-05-19 12:59:30 +0100

    add credit to transaction when initialising - Red

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -8,4 +8,9 @@ describe Transaction do
     transaction = Transaction.new(100) 
     expect(transaction.balance).to eq 100
   end
+
+  it 'should initialise with credit properties' do
+    transaction = Transaction.new(100, 50) 
+    expect(transaction.credit).to eq 50
+  end
 end
